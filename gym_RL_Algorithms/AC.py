72e9e30a2049e4a330aa2b67a316d1944fc96904
kick9850 <jungkim9898@gmail.com>
2023-05-29 15:09:14 +0900

    Update code

--- gym_RL_Algorithms/AC.py
+++ gym_RL_Algorithms/AC.py
@@ -13,12 +13,12 @@ eps = np.finfo(np.float32).eps.item()
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
 class Policy(nn.Module):
-    def __init__(self, state_space, action_space,):
+    def __init__(self, state_space, action_space, hidden_size):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(state_space, 32)
+        self.fc1 = nn.Linear(state_space, hidden_size)
 
-        self.action_head = nn.Linear(32, action_space)
-        self.value_head = nn.Linear(32, 1) # Scalar Value
+        self.action_head = nn.Linear(hidden_size, action_space)
+        self.value_head = nn.Linear(hidden_size, 1) # Scalar Value
 
         self.save_actions = []
         self.rewards = []
@@ -31,52 +31,66 @@ class Policy(nn.Module):
 
         return F.softmax(action_score, dim=-1), state_value
 
-model = Policy()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-def plot(steps):
-    ax = plt.subplot(111)
-    ax.cla()
-    ax.grid()
-    ax.set_title('Training')
-    ax.set_xlabel('Episode')
-    ax.set_ylabel('Run Time')
-    ax.plot(steps)
-    RunTime = len(steps)
+class Agent():
+    def __init__(self,
+                 state_space,
+                 action_space,
+                 hidden_size,
+                 lr,
+                 gamma):
 
-def select_action(state):
-    state = torch.from_numpy(state).float()
-    probs, state_value = model(state)
-    m = Categorical(probs)
-    action = m.sample()
-    model.save_actions.append(SavedAction(m.log_prob(action), state_value))
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden_size = hidden_size
+        self.learning_rate = lr
+        self.gamma = gamma
+        self.model = Policy(self.state_space, self.action_space,self.hidden_size)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
-    return action.item()
+    def plot(self, steps):
+        ax = plt.subplot(111)
+        ax.cla()
+        ax.grid()
+        ax.set_title('Training')
+        ax.set_xlabel('Episode')
+        ax.set_ylabel('Run Time')
+        ax.plot(steps)
+        RunTime = len(steps)
+
+    def select_action(self, state):
+        state = torch.from_numpy(state).float()
+        probs, state_value = self.model(state)
+        m = Categorical(probs)
+        action = m.sample()
+        self.model.save_actions.append(SavedAction(m.log_prob(action), state_value))
+
+        return action.item()
 
 
-def finish_episode():
-    R = 0
-    save_actions = model.save_actions
-    policy_loss = []
-    value_loss = []
-    rewards = []
+    def finish_episode(self):
+        R = 0
+        save_actions = self.model.save_actions
+        policy_loss = []
+        value_loss = []
+        rewards = []
 
-    for r in model.rewards[::-1]:
-        R = r + gamma * R
-        rewards.insert(0, R)
+        for r in self.model.rewards[::-1]:
+            R = r + self.gamma * R
+            rewards.insert(0, R)
 
-    rewards = torch.tensor(rewards)
-    rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
 
-    for (log_prob , value), r in zip(save_actions, rewards):
-        reward = r - value.item()
-        policy_loss.append(-log_prob * reward)
-        value_loss.append(F.smooth_l1_loss(value, torch.tensor([r])))
+        for (log_prob , value), r in zip(save_actions, rewards):
+            reward = r - value.item()
+            policy_loss.append(-log_prob * reward)
+            value_loss.append(F.smooth_l1_loss(value, torch.tensor([r])))
 
-    optimizer.zero_grad()
-    loss = torch.stack(policy_loss).sum() + torch.stack(value_loss).sum()
-    loss.backward()
-    optimizer.step()
+        self.optimizer.zero_grad()
+        loss = torch.stack(policy_loss).sum() + torch.stack(value_loss).sum()
+        loss.backward()
+        self.optimizer.step()
 
-    del model.rewards[:]
-    del model.save_actions[:]
+        del self.model.rewards[:]
+        del self.model.save_actions[:]
