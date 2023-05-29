1c72928b2cbc0ca6c8d008cf95768967ae206337
kick9850 <jungkim9898@gmail.com>
2023-05-29 18:22:46 +0900

    delete Git_code Using pip

---
+++ id_mujoco/test_multi.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+
+env = gym.make("FetchReach-v2")
+env.reset()
+obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+# The following always has to hold:
+assert reward == env.compute_reward(obs["achieved_goal"], obs["desired_goal"], info)
+assert truncated == env.compute_truncated(obs["achieved_goal"], obs["desired_goal"], info)
+assert terminated == env.compute_terminated(obs["achieved_goal"], obs["desired_goal"], info)
+
+# However goals can also be substituted:
+substitute_goal = obs["achieved_goal"].copy()
+substitute_reward = env.compute_reward(obs["achieved_goal"], substitute_goal, info)
+substitute_terminated = env.compute_terminated(obs["achieved_goal"], substitute_goal, info)
+substitute_truncated = env.compute_truncated(obs["achieved_goal"], substitute_goal, info)
